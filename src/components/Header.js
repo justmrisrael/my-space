f15262aabc8539cfd3fca2a751ea91076f65a4ac
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2021-11-24 07:01:14 -0600

    master

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,22 @@ import styled from 'styled-components'
 
 function Header() {
     return(
-        <Israel>
-            Israel Musa
-        </Israel>
+        <Home>
+            <a href = "#">Israel Musa</a>
+        </Home>
     )
 }
 export default Header
 
 
-const Israel = styled.div`
-    font-family: 'Geo';
-    font-size: 4em;
-    color: #C7CCDB;
+const Home = styled.div`
     display: flex;
     justify-content: Left;
+    a {
+        text-align: start;
+        font-family: 'Geo';
+        font-size: 4em;
+        font-weight: normal;
+        color: #C7CCDB;
+    }
 `
