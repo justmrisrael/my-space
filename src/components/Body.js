822d9da83c41d42e3f2d8c74101377b91a94002a
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2021-11-26 14:16:40 -0600

    master

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,22 +18,22 @@ const Home = styled.div`
   position: absolute;
   top: 100px;
   margin-left: 2em;
-  margin-top: 2em;
+  margin-top: 4em;
 `;
 const TextContainer = styled.div`
-  position: relative;
   a {
     font-family: "Geo";
     font-size: 4em;
     font-weight: normal;
-    color: #c7ccdb;
+    color: #C7CCDB;
   }
 `;
 const StaticWords = styled.div`
-  margin-left: 1em;
+  position: absolute;
+  margin-left: 0;
   a {
       font-family: "Montserrat";
-      font-size: 2em;
+      font-size: 1em;
       color: #F7C59F;
   }
 
