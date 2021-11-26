be285509c8a703c2a75ab68bbda93959f83cd6df
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2021-11-26 13:30:49 -0600

    master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import './App.css';
+import "./App.css";
 import DotRing from "./components/DotRing/DotRing";
 import Header from "./components/Header";
 import { MouseContext } from "./context/mouse-context";
@@ -8,20 +8,17 @@ function App() {
   const { cursorChangeHandler } = useContext(MouseContext);
   return (
     <div className="App">
+      <Header />
       <DotRing />
       <div className="container">
         <div
           onMouseEnter={() => cursorChangeHandler("hovered")}
           onMouseLeave={() => cursorChangeHandler("")}
-        >
-            <Header />
-        </div>
+        ></div>
       </div>
-      <div className="container" style={{ background: "#2A324B" }}>
-    </div>
+      <div className="container" style={{ background: "#2A324B" }}></div>
     </div>
   );
 }
-
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,39 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 function Header() {
-    return(
-        <Home>
-            <a href = "#">Israel Musa</a>
-        </Home>
-    )
+  return (
+    <Home>
+      <TextContainer>
+        <a href="#">Israel Musa</a>
+      </TextContainer>
+      <StaticWords>
+          <a>front end developer</a>
+      </StaticWords>
+    </Home>
+  );
 }
-export default Header
-
+export default Header;
 
 const Home = styled.div`
-    position: relative;
-    justify-content: Left;
-    a {
-        text-align: start;
-        font-family: 'Geo';
-        font-size: 4em;
-        font-weight: normal;
-        color: #C7CCDB;
-    }
+  position: absolute;
+  top: 100px;
+`;
+const TextContainer = styled.div`
+  position: relative;
+  top: 0;
+  a {
+    font-family: "Geo";
+    font-size: 4em;
+    font-weight: normal;
+    color: #c7ccdb;
+  }
+`;
+const StaticWords = styled.div`
+  position: flex;
+  a {
+      font-family: "Montserrat";
+      font-size: 2em;
+      color: #F7C59F;
+  }
+
 `
