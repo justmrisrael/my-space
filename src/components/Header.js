7b4bbe045b450e24f53ca452c89f5c131b3b6860
justmrisrael <83407369+justmrisrael@users.noreply.github.com>
2021-11-28 07:20:39 -0600

    master

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import styled from "styled-components";
 
 function Header() {
   return (
     <NavBar>
-        <Logo>
-      <a href="#">IM</a>
+      <Logo>
+        <a href="#">IM</a>
       </Logo>
-
+      <Menu>
+        <a href="#">Home</a>
+        <a href="#">Projects</a>
+        <a href="#">Resume</a>
+        <a href="#">Contact</a>
+      </Menu>
     </NavBar>
-  )
+  );
 }
-export default Header
+export default Header;
 
 const NavBar = styled.div`
-    position: absolute;
-`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  top: 0; 
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
 const Logo = styled.div`
-    position: relative;
-    border: 2px solid white;
-    margin-top: 2em;
-    margin-left: 2em;
-    cursor: pointer;
-    a{
-        font-family: "Geo";
-        color: #E1E5EE;
-        font-size: 2em;
-        font-weight: normal;
-    }
-`
+  position: relative;
+  border: 2px solid white;
+  margin-top: 2em;
+  margin-left: 2em;
+  cursor: pointer;
+  a {
+    font-family: "Geo";
+    color: #e1e5ee;
+    font-size: 2em;
+    font-weight: normal;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  flex: 1;
+  margin-right: 1em;
+
+  a {
+    font-family: "Helvetica Neue";
+    font-weight: bold;
+    color: #e1e5ee;
+    text-transform: none;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
